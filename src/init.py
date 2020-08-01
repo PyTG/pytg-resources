0c96c29f32f75e616da8f96d0288a251ff7b2255
aegroto <aegroto@protonmail.com>
2020-08-02 01:22:14 +0200

    added configurable manager

--- src/init.py
+++ src/init.py
@@ -1,17 +1,33 @@
 import logging
 
-from modules.resources.YamlResourcesManager import YamlResourcesManager
+from modules.pytg.ModulesLoader import ModulesLoader
+
+from .YamlResourcesManager import YamlResourcesManager
+
+MANAGER_CLASS = None
 
 def initialize():
     logging.info("Initializing resources module...")
 
-    YamlResourcesManager.initialize()
+    config_manager = ModulesLoader.load_manager("config")
+
+    settings = config_manager.load_settings_file("resources")
+    
+    MANAGER_CLASS = __get_manager_class(settings["format"])
+
+    MANAGER_CLASS.initialize()
 
 def connect():
     pass
 
 def load_manager():
-    return YamlResourcesManager.load()
+    return MANAGER_CLASS.load()
+
+def __get_manager_class(format):
+    if format == "yaml":
+        return YamlResourcesManager
+
+    return None
 
 def depends_on():
-    return []
+    return ["config"]
