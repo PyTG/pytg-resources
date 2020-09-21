17d8badeb49b83059932b61e3dfc667f1c291104
aegroto <aegroto@protonmail.com>
2020-09-21 11:43:55 +0200

    new loaders system

--- src/init.py
+++ src/init.py
@@ -2,32 +2,22 @@ import logging
 
 from modules.pytg.ModulesLoader import ModulesLoader
 
-from .YamlResourcesManager import YamlResourcesManager
-
-MANAGER_CLASS = None
+from .ResourcesManager import ResourcesManager
 
 def initialize():
     logging.info("Initializing resources module...")
 
     config_manager = ModulesLoader.load_manager("config")
 
-    settings = config_manager.load_settings_file("resources")
-    
-    MANAGER_CLASS = __get_manager_class(settings["format"])
+    settings = config_manager.load_settings("resources")
 
-    MANAGER_CLASS.initialize()
+    ResourcesManager.initialize()
 
 def connect():
     pass
 
 def load_manager():
-    return MANAGER_CLASS.load()
-
-def __get_manager_class(format):
-    if format == "yaml":
-        return YamlResourcesManager
-
-    return None
+    return ResourcesManager.load()
 
 def depends_on():
     return ["config"]
